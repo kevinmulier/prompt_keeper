cc5585ac2ee0f56d41655aaebd6089178a71e919
kevinmulier <kevin.mulier@outlook.fr>
2023-08-15 16:26:38 +0200

    Added default port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`The server is now operational in ${process.env.NODE_ENV} mode on port ${PORT}`);
