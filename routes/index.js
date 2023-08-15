919a9f4c4cc8415abef3a2c7e33e3726987248c2
kevinmulier <kevin.mulier@outlook.fr>
2023-08-15 23:38:10 +0200

    Added "/" routes

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ const router = express.Router();
 // Desc : Login / Landing page
 // Route : GET /
 router.get("/", (req, res) => {
-  res.send("Login");
+  res.render("login");
 });
 
 // Desc : Dashboard
 // Route : GET /dashboard
 router.get("/dashboard", (req, res) => {
-  res.send("Dashboard");
+  res.render("dashboard");
 });
 
 module.exports = router;
