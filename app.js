767ca7ecb113c53648e9a484fc8a074173d4a3b0
kevinmulier <kevin.mulier@outlook.fr>
2023-08-15 23:38:19 +0200

    Added static folder

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
@@ -19,6 +20,9 @@ if (process.env.NODE_ENV === "dev") {
 // Handlebars
 app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
+
+// Static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/", require("./routes/index"));
