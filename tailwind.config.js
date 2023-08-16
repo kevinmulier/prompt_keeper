38c70b65b572f8fbec841213b7b1e897a852551b
kevinmulier <kevin.mulier@outlook.fr>
2023-08-16 19:14:19 +0200

    Added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./views/**/*.hbs"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        violet: {
+          50: "#e8eaff",
+          100: "#d5d8ff",
+          200: "#b3b6ff",
+          300: "#8585ff",
+          400: "#6655ff",
+          500: "#562fff",
+          600: "#4e0cff",
+          700: "#4c02ff",
+          800: "#3c06cd",
+          900: "#33109f",
+          950: "#180745",
+        },
+      },
+    },
   },
   plugins: [],
 };
