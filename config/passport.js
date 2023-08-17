5583157f0b84c2b514f72fa41dffbcd5fb30ca36
kevinmulier <kevin.mulier@outlook.fr>
2023-08-17 12:51:15 +0200

    Added passport (google oauth 2.0)

---
+++ config/passport.js
@@ -0,0 +1,53 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const User = require("../models/User");
+
+module.exports = function (passport) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "/auth/google/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        const newUser = {
+          googleID: profile.id,
+          displayName: profile.displayName,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          image: profile.photos[0].value,
+        };
+
+        try {
+          let user = await User.findOne({ googleId: profile.id });
+
+          if (user) {
+            done(null, user);
+          } else {
+            user = await User.create(newUser);
+            done(null, user);
+          }
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    )
+  );
+
+  passport.serializeUser((user, cb) => {
+    process.nextTick(function () {
+      return cb(null, {
+        id: user.id,
+        username: user.username,
+        picture: user.picture,
+      });
+    });
+  });
+
+  passport.deserializeUser((user, cb) => {
+    process.nextTick(function () {
+      return cb(null, user);
+    });
+  });
+};
