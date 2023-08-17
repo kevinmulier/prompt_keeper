2830a50edd1fd2c46e2637166beb40df40e46a91
kevinmulier <kevin.mulier@outlook.fr>
2023-08-17 12:52:41 +0200

    Added auth route (google)

---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+
+// Desc : Auth (Google)
+// Route : GET /auth/google
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+
+// Desc : Google auth callback
+// Route : GET /auth/google/callback
+router.get("/google/callback", passport.authenticate("google", { failureRedirect: "/" }), (req, res) => {
+  res.redirect("/dashboard");
+});
+
+module.exports = router;
