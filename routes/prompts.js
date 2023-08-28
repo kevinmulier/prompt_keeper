ff31f9e295dd17a5ae5ca1059be9dd2456af199f
kevinmulier <kevin.mulier@outlook.fr>
2023-08-29 00:34:13 +0200

    Added CREATE to add new prompt

--- routes/prompts.js
+++ routes/prompts.js
@@ -4,10 +4,23 @@ const { ensureAuth } = require("../middleware/auth");
 
 const Prompt = require("../models/Prompt");
 
-// Desc : Add page
+// Desc : Add new prompt page
 // Route : GET /prompts/add
 router.get("/add", ensureAuth, (req, res) => {
   res.render("prompts/add");
+});
+
+// Desc : Process new prompt
+// Route : POST /prompts
+router.post("/", ensureAuth, async (req, res) => {
+  try {
+    req.body.user = req.user.id;
+    await Prompt.create(req.body);
+    res.redirect("/dashboard");
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
 });
 
 module.exports = router;
